4de188cfec9d2e4cda4d50c692256864e95090f1
david-wendt <dwendt@stanford.edu>
2023-11-01 17:36:37 -0700

    Outline strategy in baseline_agent.py

--- agents/baseline_agent.py
+++ agents/baseline_agent.py
@@ -4,6 +4,18 @@ class BaselineAgent(AbstractAgent):
     ''' Deterministic baseline agent. Should play by a reasonable, but
     not too complicated, strategy. '''
     def choose_card(self, legal_cards, hand_state):
+        ''' 
+        Choose a card from the set of legal cards according to a simple, reasonable strategy.
+        
+        Strategy outline:
+            If your PARTNER is WINNING, or you cannot beat the currently winning card,
+                then play your worst card (tiebreak randomly if non-trump and non-lead-suit)
+            Otherwise (i.e. if you CAN WIN but your PARTNER is NOT WINNIG),
+                then play the worst card that will win you the trick (as of the currently cards on the table)
+                
+        return: card (Card): Choice of card to play.
+        '''
+        trump_suit, trick = hand_state.trump_suit, hand_state.trick
         raise NotImplementedError('Need to implement strategy for the deterministic baseline agent.')
     
     def get_agent_type_str(self):
